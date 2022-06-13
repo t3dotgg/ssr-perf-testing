0b7a1e6b88dbc6528573ca41e27dd743ae80f084
Theo Browne <me@t3.gg>
2022-06-13 15:35:18 -0700

    Cleanup

--- remix/app/components/render-content.tsx
+++ remix/app/components/render-content.tsx
@@ -2,10 +2,8 @@ export default function Content(props: any) {
   const renderedAt = new Date();
   const timeElapsed = renderedAt.getTime() - props.startedAt;
 
-  console.log("content?", props);
-
+  const requestLatency = "request-latency";
   const fillId = "fill-with-render-time";
-
   const perfFullTime = "perf-full";
 
   return (
@@ -18,25 +16,35 @@ export default function Content(props: any) {
       <h2>
         Request that generated this page RENDERED at: {renderedAt.getTime()}
       </h2>
-      <h2>Time to respond on backend: {props.endedAt - props.startedAt}</h2>
-      <h2>Time to render on backend: {timeElapsed}</h2>
+      <h2>Time to respond on backend: {props.endedAt - props.startedAt}ms</h2>
+      <h2>Time to render on backend: {timeElapsed}ms</h2>
+      <h2>
+        Request latency (request to edge time, requires ssr):{" "}
+        <span id={requestLatency} />
+        ms
+      </h2>
       <h2>
         Time from server request to CLIENT render: <span id={fillId} />
+        ms
+      </h2>
+      <h2>
+        Request-to-paint time (Chrome only): <span id={perfFullTime} />
+        ms
       </h2>
       <script>{`
       const currentTime = new Date();
-      const timeElapsed = currentTime.getTime() - ${props.startedAt};
-      console.log(1);
-      document.getElementById(\`${fillId}\`).innerHTML = timeElapsed;
-      `}</script>
-      <h2>
-        Request-to-paint time (Chrome only): <span id={perfFullTime} />
-      </h2>
-      <script>{`
-      const ct = new Date();
-      const te = ct - window.performance.timing.requestStart;
-      console.log(1);
-      document.getElementById(\`${perfFullTime}\`).innerHTML = te;
+
+      // Server latency
+      const latencyTime = ${props.startedAt} - window.performance.timing.requestStart;
+      document.getElementById(\`${requestLatency}\`).innerHTML = latencyTime;
+
+      // Request to paint time
+      const timeFromRequestReceived = currentTime.getTime() - ${props.startedAt};
+      document.getElementById(\`${fillId}\`).innerHTML = timeFromRequestReceived;
+
+      // round trip time
+      const fullTime = currentTime - window.performance.timing.requestStart;
+      document.getElementById(\`${perfFullTime}\`).innerHTML = fullTime;
       `}</script>
     </div>
   );
