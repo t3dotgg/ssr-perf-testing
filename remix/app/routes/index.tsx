c7a144064918721ccd5697e7bd1e3f820d96014b
Theo Browne <me@t3.gg>
2022-06-13 10:36:45 -0700

    style up home

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,8 +1,20 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <a href="/3p-direct">Hit github directly through loader</a>
+      <div style={{ marginTop: "1rem" }} />
       <a href="/3p-indirect">Hit github indirectly through cdn cache</a>
+      <div style={{ marginTop: "1rem" }} />
+      <a href="/3p-cached-page">
+        Hit Remix cached page w/ direct github loader
+      </a>
     </div>
   );
 }
