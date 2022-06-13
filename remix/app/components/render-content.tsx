1227687d6f218170806f4c24e287a7f4f8a9e988
Theo Browne <me@t3.gg>
2022-06-13 10:56:36 -0700

    Render on client time

--- remix/app/components/render-content.tsx
+++ remix/app/components/render-content.tsx
@@ -4,6 +4,8 @@ export default function Content(props: any) {
 
   console.log("content?", props);
 
+  const fillId = "fill-with-render-time";
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Profile: {props.login}</h1>
@@ -11,8 +13,20 @@ export default function Content(props: any) {
         Request that generated this page was STARTED at: {props.startedAt}
       </h2>
       <h2>Request that generated this page COMPLETED at: {props.endedAt}</h2>
+      <h2>
+        Request that generated this page RENDERED at: {renderedAt.getTime()}
+      </h2>
       <h2>Time to respond on backend: {props.endedAt - props.startedAt}</h2>
-      <h2>Time to render: {timeElapsed}</h2>
+      <h2>Time to render on backend: {timeElapsed}</h2>
+      <h2>
+        Time from initial request to CLIENT render: <div id={fillId} />
+      </h2>
+      <script>{`
+      const currentTime = new Date();
+      const timeElapsed = currentTime.getTime() - ${props.startedAt};
+      console.log(1);
+      document.getElementById(\`${fillId}\`).innerHTML = timeElapsed;
+      `}</script>
     </div>
   );
 }
